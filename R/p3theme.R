39e5d856762a4734b3ec3bbd4c98a7ea6b53401a
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-03-31 12:22:02 -0700

    initial commit

---
+++ R/p3theme.R
@@ -0,0 +1,28 @@
+
+#' Add p3 theme
+#'
+#' @param ggplot_obj A ggplot object
+#' @return A ggplot object with p3 ggthemes
+#' @importFrom ggplot2 scale_fill_manual
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 element_text
+#' @export
+#'
+
+add_p3theme <- function(ggplot_obj) {
+
+    #527aaf - fractal blue
+    #ffbd59 - prism peach
+    #ff914d - organizing orange
+
+    ggplot_obj +
+        scale_fill_manual(values = c("#527aaf","#ff914d","#ffbd59"))
+        theme(text = element_text(
+                        size = 12,
+                        family = "Helvetica Neue"),
+                        axis.title.y = element_text(face = "bold", size = 12),
+                        axis.title.x = element_text(face = "bold", size = 12),
+                        axis.text.x = element_text(size = 10),
+                        plot.title = element_text(face = "bold"),
+                        legend.title = element_text(size = 11))
+}
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # p3themes
+
+The goal of GetAboutPages is to ...
+
+## Installation 
+
+```r
+## Install the current development version from GitHub
+
+if (!require("devtools")) {
+
+    install.packages("devtools")
+    
+    }
+
+install_github("p3lab/p3themes", dependencies = TRUE)
+```
+
+## Usage 
+
+This is a basic example which shows you how to solve a common problem:
+
+```r
+library(p3themes)
+## basic example code
+```
